42f599d2be0582ccc47602891be0559d33b8662b
Nata <Nata@mail.ru>
2020-06-03 16:11:09 +0200

    add Application

---
+++ src/main/java/ua/com/foxminded/division/Application.java
@@ -0,0 +1,12 @@
+package ua.com.foxminded.division;
+
+public class Application {
+
+	public static void main(String[] args) {
+		final int divindend = 78945;
+		final int divinder = 4;
+		Division division = new Division();
+		final String resultDivision = division.divideColumn(divindend, divinder);
+        System.out.println(resultDivision);	
+	}
+}
